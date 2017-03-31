450a7dc51ee9b99d7bdd30548467e9a033bc8974
theyawninglion <theyawninglion@gmail.com>
2017-03-31 09:44:50 -0600

    set up storyboard and model

---
+++ Pair/Pair/PairTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PairTableViewController.swift
+//  Pair
+//
+//  Created by Taylor Phillips on 3/31/17.
+//  Copyright © 2017 Taylor Phillips. All rights reserved.
+//
+
+import UIKit
+
+class PairTableViewController: UITableViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return <#rowCount#>
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "objectCell", for: indexPath)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}
